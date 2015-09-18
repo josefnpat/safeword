b6cd519d0bfbddee2fac63234a23ddc322d2dae1
Seppi <seppi@josefnpat.com>
2015-09-18 07:51:48 -0400

    Reordering for ldoc

--- safeword.lua
+++ safeword.lua
@@ -19,6 +19,75 @@ local safeword = {
   ]]
 }
 
+--- Instansiate a new instance of SafeWord.
+-- Some Examples:<br/>
+-- <code>sw = safeword.new()<br/>
+-- <br/>
+-- sw = safeword.new({<br/>
+--   horizontalAction = 0.03,<br/>
+--   verticalAction = 0.03,<br/>
+-- })<br/>
+-- <br/>
+-- sw = safeword.new{<br/>
+--   horizontalAction = 0.03,<br/>
+--   verticalAction = 0.03,<br/>
+--   horizontalTitle = 0.07,<br/>
+--   verticalTitle = 0.07,<br/>
+-- }<br/></code>
+-- @param init <i>Optional</i> A table containing initial values for easy usage. Available
+-- overrides: <code>horizontalAction</code>, <code>horizontalTitle</code>,
+-- <code>verticalAction</code> and <code>verticalTitle</code>.
+-- @return Table returns the safeword class table.
+function safeword.new(init)
+  init = init or {}
+  local self={}
+  self.draw=safeword.draw
+  self.setPreset=safeword.setPreset
+  self.getModes=safeword.getModes
+
+  self.getActionX=safeword.getActionX
+  self.getActionY=safeword.getActionY
+  self.getActionWidth=safeword.getActionWidth
+  self.getActionHeight=safeword.getActionHeight
+
+  self.getTitleX=safeword.getTitleX
+  self.getTitleY=safeword.getTitleY
+  self.getTitleWidth=safeword.getTitleWidth
+  self.getTitleHeight=safeword.getTitleHeight
+
+  self._horizontalAction=init.horizontalAction or 0.035
+  self.getHorizontalAction=safeword.getHorizontalAction
+  self.setHorizontalAction=safeword.setHorizontalAction
+  self._horizontalTitle=init.horizontalTitle or 0.07
+  self.getHorizontalTitle=safeword.getHorizontalTitle
+  self.setHorizontalTitle=safeword.setHorizontalTitle
+  self._verticalAction=init.verticalAction or 0.035
+  self.getVerticalAction=safeword.getVerticalAction
+  self.setVerticalAction=safeword.setVerticalAction
+  self._verticalTitle=init.verticalTitle or 0.07
+  self.getVerticalTitle=safeword.getVerticalTitle
+  self.setVerticalTitle=safeword.setVerticalTitle
+
+  self:setPreset()
+
+  return self
+end
+
+safeword.modes = {
+  BBC = "British Broadcasting Corporation",
+  EBU = "European Broadcasting Union",
+  XBOX = "Microsoft's Xbox game developer guidelines",
+  OUYA = "OUYA SDK Example",
+}
+
+function safeword._gcd(a,b)
+  if b ~= 0 then
+    return safeword._gcd(b, a % b)
+  else
+    return math.abs(a)
+  end
+end
+
 --- Main draw function
 -- This is the main draw function that one would add to <code>love.draw</code>.
 function safeword:draw()
@@ -66,21 +135,6 @@ function safeword:draw()
 
   love.graphics.setColor(old_color)
 end
-
-function safeword._gcd(a,b)
-  if b ~= 0 then
-    return safeword._gcd(b, a % b)
-  else
-    return math.abs(a)
-  end
-end
-
-safeword.modes = {
-  BBC = "British Broadcasting Corporation",
-  EBU = "European Broadcasting Union",
-  XBOX = "Microsoft's Xbox game developer guidelines",
-  OUYA = "OUYA SDK Example",
-}
 
 --- Get the available modes in SafeWord
 -- @return Table of strings with acronyms.
@@ -207,60 +261,6 @@ end
 --- Get the calculated height of the title safe area.
 function safeword:getTitleHeight()
   return love.graphics.getHeight()*(1-self:getHorizontalTitle()*2)
-end
-
---- Instansiate a new instance of SafeWord.
--- Some Examples:<br/>
--- <code>sw = safeword.new()<br/>
--- <br/>
--- sw = safeword.new({<br/>
---   horizontalAction = 0.03,<br/>
---   verticalAction = 0.03,<br/>
--- })<br/>
--- <br/>
--- sw = safeword.new{<br/>
---   horizontalAction = 0.03,<br/>
---   verticalAction = 0.03,<br/>
---   horizontalTitle = 0.07,<br/>
---   verticalTitle = 0.07,<br/>
--- }<br/></code>
--- @param init <i>Optional</i> A table containing initial values for easy usage. Available
--- overrides: <code>horizontalAction</code>, <code>horizontalTitle</code>,
--- <code>verticalAction</code> and <code>verticalTitle</code>.
--- @return Table returns the safeword class table.
-function safeword.new(init)
-  init = init or {}
-  local self={}
-  self.draw=safeword.draw
-  self.setPreset=safeword.setPreset
-  self.getModes=safeword.getModes
-
-  self.getActionX=safeword.getActionX
-  self.getActionY=safeword.getActionY
-  self.getActionWidth=safeword.getActionWidth
-  self.getActionHeight=safeword.getActionHeight
-
-  self.getTitleX=safeword.getTitleX
-  self.getTitleY=safeword.getTitleY
-  self.getTitleWidth=safeword.getTitleWidth
-  self.getTitleHeight=safeword.getTitleHeight
-
-  self._horizontalAction=init.horizontalAction or 0.035
-  self.getHorizontalAction=safeword.getHorizontalAction
-  self.setHorizontalAction=safeword.setHorizontalAction
-  self._horizontalTitle=init.horizontalTitle or 0.07
-  self.getHorizontalTitle=safeword.getHorizontalTitle
-  self.setHorizontalTitle=safeword.setHorizontalTitle
-  self._verticalAction=init.verticalAction or 0.035
-  self.getVerticalAction=safeword.getVerticalAction
-  self.setVerticalAction=safeword.setVerticalAction
-  self._verticalTitle=init.verticalTitle or 0.07
-  self.getVerticalTitle=safeword.getVerticalTitle
-  self.setVerticalTitle=safeword.setVerticalTitle
-
-  self:setPreset()
-
-  return self
 end
 
 --- Get the currently set horizontal action safe area percentage.
